dd6ff902da4a6dcd122eefc8689eb0d77110f08d
Renan Oliveira <renancesar03@gmail.com>
2023-01-03 00:15:13 -0300

    inicializa projeto

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import io from 'socket.io-client'
+
+// const socket = io('http://localhost:3333')
+
+function App() {
+    return (<h1>Hello World</h1>);
+}
+
+export default App
