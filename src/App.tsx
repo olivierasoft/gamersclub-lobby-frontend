5d4fddea6ec1e6d49359fa412c494c0d982bede5
Renan Oliveira <renancesar03@gmail.com>
2023-01-04 22:31:56 -0300

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css'
-import io from 'socket.io-client'
+import {Login} from "./pages/Login/Login";
 
 // const socket = io('http://localhost:3333')
 
 function App() {
-    return (<h1>Hello World</h1>);
+    return <Login/>;
 }
 
 export default App
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      navbar: '#1f1f1f',
+      white: '#fff',
+      optionHover: 'rgba(255, 255, 255, 0.04)'
+    },
     extend: {},
   },
   plugins: [],
